4666ddfcfe67a32f68f280257d444a1ae456b8e2
Florin Cosmin <florincosmin@fastmail.com>
2018-02-07 13:09:40 +0100

    updated api key and tutorial code

--- src/js/modules/google-auth-demo.js
+++ src/js/modules/google-auth-demo.js
@@ -1,7 +1,8 @@
+// tutorial here: https://developers.google.com/drive/v3/web/quickstart/js
+
 // Client ID and API key from the Developer Console
-const CLIENT_ID = '635136783186-4ql36uathmopkbc8maeg020vrfnfpngj.apps.googleusercontent.com';
-// const API_KEY = 'AIzaSyDxZC99hAP5kRXdcb4iyEHN7kL0QpfmrTQ';
-const API_KEY = 'AIzaSyC3n0E6C6tWWf_npDyyCEGwfobqwJU2jUU';
+const CLIENT_ID = '378789089814-77dah8n0j12glfhi2aa1ic73enacfnks.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyAM1Ktgs9e1YlIdLwcgiH_aHNN5hFdEkMw';
 
 // Array of API discovery doc URLs for APIs used by the quickstart
 const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
@@ -10,52 +11,31 @@ const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/r
 // included, separated by spaces.
 const SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
 
-// var authorizeButton = document.getElementById('authorize-button');
-// var signoutButton = document.getElementById('signout-button');
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-// export function handleClientLoad() {
-//   gapi.load('client:auth2', initClient);
-// }
+export const handleClientLoad = (callback) => {
+  gapi.load('client:auth2', callback);
+};
 
 /**
  *  Initializes the API client library and sets up sign-in state
  *  listeners.
  */
-export function initClient () {
-  gapi.client.init({
+export const initClient = () => {
+  return gapi.client.init({
     apiKey: API_KEY,
     clientId: CLIENT_ID,
     discoveryDocs: DISCOVERY_DOCS,
     scope: SCOPES
-  }).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    // authorizeButton.onclick = handleAuthClick;
-    // signoutButton.onclick = handleSignoutClick;
   });
-}
+};
 
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-export function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    // authorizeButton.style.display = 'none';
-    // signoutButton.style.display = 'block';
-    console.log('hide authorize button and show sign out button');
-    listFiles();
-  } else {
-    // authorizeButton.style.display = 'block';
-    // signoutButton.style.display = 'none';
-    console.log('show authorize button and hide sign out button');
-  }
+// listen for signin state changes
+export function listenForSignInChanges(callback) {
+
+  // Listen for sign-in state changes.
+  gapi.auth2.getAuthInstance().isSignedIn.listen(callback);
+
+  // Handle the initial sign-in state.
+  callback(gapi.auth2.getAuthInstance().isSignedIn.get());
 }
 
 /**
@@ -88,20 +68,9 @@ export function appendPre(message) {
 /**
  * Print files.
  */
-export function listFiles() {
-  gapi.client.drive.files.list({
-    'pageSize': 10,
+export function getFiles(pageSize) {
+  return gapi.client.drive.files.list({
+    'pageSize': pageSize,
     'fields': 'nextPageToken, files(id, name)'
-  }).then(function (response) {
-    appendPre('Files:');
-    var files = response.result.files;
-    if (files && files.length > 0) {
-      for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        appendPre(file.name + ' (' + file.id + ')');
-      }
-    } else {
-      appendPre('No files found.');
-    }
   });
 }
--- src/js/pages/login.js
+++ src/js/pages/login.js
@@ -7,10 +7,9 @@ import Navbar from '../components/navbar';
 class IndexPage extends Component {
 
   state = {
-    hasSlider: false,
-    modalType: true, // modal dialog type. true: can be manually hidden, false: cannot be manually hidden
-    dialogType: -1, // settings dialog on/off [-1 none, 0 settings, 1 bluetooth, 2 logout, 3 relogin, 4 go offline]
-    showModal: false,
+    files: [],
+    initApi: false,
+    signedIn: false,
 
     // TODO: use global state instead
     error: {
@@ -25,6 +24,7 @@ class IndexPage extends Component {
     let brand = <p class='navbar-text'>DriveApiDemo <code> {version}</code></p>;
 
     let pageContentStyles = 'padding-bottom: 51px';
+    let { initApi, signedIn, files } = state;
 
     return (
       <div class='index-view'>
@@ -36,19 +36,60 @@ class IndexPage extends Component {
         <div id='page-content' class='container' style={pageContentStyles}>
           <div class='row'>
             <div>Hello world!</div>
-            <button type='button' class='btn btn-default' onClick={this.onAuth}>Google Auth</button>
+            {!initApi && <button type='button' class='btn btn-default' onClick={this.initApi}>Init Google API</button>}
+            <button type='button' class='btn btn-default' id='authorize-button' style={signedIn ? 'display: none;' : 'display: block;'} onClick={this.handleAuth}>Sign in</button>
+            <button type='button' class='btn btn-default' id='signout-button' style='display: none;' onClick={this.handleSignOut}>Sign Out</button>
+          </div>
+          <div class='row'>
+            {files.length && files.map((f, idx) => {
+              return <ul key={f.id} >name: {f.name} - id = {f.id} </ul>;
+            })}
           </div>
         </div>
       </div>
     );
   }
 
-  onAuth = (ev) => {
-    gapi.load('client:auth2', gapiDemo.initClient);
+  handleAuth = (ev) => {
+    gapi.auth2.getAuthInstance().signIn();
   }
 
-  componentDidMount() {
-    // gapiDemo.handleClientLoad();
+  handleSignOut = (ev) => {
+    gapi.auth2.getAuthInstance().signOut();
+  }
+
+  updateSigninStatus = (isSignedIn) => {
+    this.setState({
+      signedIn: isSignedIn
+    });
+  }
+
+  getFiles() {
+    return gapiDemo
+      .getFiles(10)
+      .then(response => {
+        // console.log(response);
+        return this.setState({
+          files: response.result.files
+        });
+      });
+  }
+
+  initApi = () => {
+    gapiDemo.handleClientLoad(() => {
+      gapiDemo
+        .initClient()
+        .then(() => {
+          let isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+          this.updateSigninStatus(isSignedIn);
+
+          if (isSignedIn) {
+            this.getFiles();
+          }
+
+          this.setState({ initApi: true });
+        });
+    });
   }
 };
 
