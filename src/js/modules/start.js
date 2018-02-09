241a856fcc23a3f36cec1c3df8cdd7f73e9533b0
Florin Cosmin <florincosmin@fastmail.com>
2018-02-09 10:56:51 +0100

    better async code

--- src/js/modules/start.js
+++ src/js/modules/start.js
@@ -13,7 +13,13 @@ import AppRouterFactory from '../routes/global';
 // import { STORAGE_KEY, UPDATE_URL } from '../redux/types';
 import { STORAGE_KEY, SIGNED_IN } from '../redux/types';
 
+import pdfjsLib from 'pdfjs-dist';
+
 export default () => {
+
+  // Setting worker path to worker bundle.
+  pdfjsLib.PDFJS.workerSrc = './src/js/vendor/pdf.worker.js';
+
   let initialState;
   try {
 
@@ -42,18 +48,44 @@ export default () => {
   // create app router
   const AppRouter = AppRouterFactory(_, Backbone);
 
+  // defaults
+  let fileId;
+  var signedIn = false;
+
+  try {
+    let stateParam = JSON.parse(decodeURIComponent(location.search.slice(7)));
+    if (stateParam && stateParam.ids && stateParam.ids.length === 1) {
+      fileId = stateParam.ids[0];
+      store.dispatch({
+        type: 'UPDATE_FILE_ID',
+        payload: fileId
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  render(
+    <Provider store={store}>
+      <App Router={AppRouter} />
+    </Provider>,
+    document.getElementById('app')
+  );
+
+  // hide spinner
+  store.dispatch({ type: 'HIDE_SPINNER' });
+
   // TODO: google api loading should be synchronous
   gapiDemo.handleClientLoad(() => {
     gapiDemo
       .initClient()
       .then(() => {
-        let signedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+        signedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
         store.dispatch({
           type: SIGNED_IN,
           payload: gapi.auth2.getAuthInstance().isSignedIn.get()
         });
 
-        // listen for sign in changes
         gapiDemo.listenForSignInChanges(() => {
           store.dispatch({
             type: SIGNED_IN,
@@ -61,33 +93,37 @@ export default () => {
           });
         });
 
-        try {
-          let fileId;
-          let stateParam = JSON.parse(decodeURIComponent(location.search.slice(7)));
-          if (stateParam && stateParam.ids && stateParam.ids.length === 1) {
-            fileId = stateParam.ids[0];
-            store.dispatch({
-              type: 'UPDATE_FILE_ID',
-              payload: fileId
+        if (signedIn && fileId) {
+          let accessToken = gapiDemo.getAccessToken();
+          let pdfUrl = `https://www.googleapis.com/drive/v3/files/${this.props.fileId}?alt=media&&access_token=${accessToken}`;
+          let loadingTask = pdfjsLib.getDocument(pdfUrl);
+
+          // TODO: dispatch action with pdfDocument as payload
+          loadingTask.promise.then(pdfDocument => {
+
+            // request the first page only
+            return pdfDocument.getPage(1).then(pdfPage => {
+
+              // Display page on the existing canvas with 100% scale.
+              var viewport = pdfPage.getViewport(1.0);
+              var canvas = document.getElementById('the-canvas');
+
+              canvas.width = viewport.width;
+              canvas.height = viewport.height;
+              // canvas.width = 640;
+              // canvas.height = 480;
+
+              var ctx = canvas.getContext('2d');
+              var renderTask = pdfPage.render({
+                canvasContext: ctx,
+                viewport: viewport
+              });
+              return renderTask.promise;
             });
-
-            // file id but not signed in
-            if (!signedIn) {
-              gapiDemo.signIn();
-            }
-          }
-        } catch (e) {
-          console.log(e);
+          }).catch(function (reason) {
+            console.error('Error: ' + reason);
+          });
         }
-
-        render(
-          <Provider store={store}>
-            <App Router={AppRouter} />
-          </Provider>,
-          document.getElementById('app')
-        );
-
-        store.dispatch({ type: 'HIDE_SPINNER' });
       });
   });
 };
--- src/js/pages/login.js
+++ src/js/pages/login.js
@@ -1,17 +1,10 @@
 import { h, Component } from 'preact';
 import { connect } from 'preact-redux';
-import * as gapiDemo from '../modules/google-auth-demo';
+// import * as gapiDemo from '../modules/google-auth-demo';
 
 import Navbar from '../components/navbar';
 
 // import 'pdfjs-dist';
-import '../vendor/gapi';
-
-import pdfjsLib from 'pdfjs-dist';
-
-// Setting worker path to worker bundle.
-// pdfjsLib.PDFJS.workerSrc = '../../build/webpack/pdf.worker.bundle.js';
-pdfjsLib.PDFJS.workerSrc = './src/js/vendor/pdf.worker.js';
 
 class IndexPage extends Component {
 
@@ -52,39 +45,6 @@ class IndexPage extends Component {
         </div>
       </div>
     );
-  }
-
-  componentDidMount () {
-    let demo = false;
-    if (this.props.signedIn && this.props.fileId && demo) {
-      let accessToken = gapiDemo.getAccessToken();
-      let pdfUrl = `https://www.googleapis.com/drive/v3/files/${this.props.fileId}?alt=media&&access_token=${accessToken}`;
-      let loadingTask = pdfjsLib.getDocument(pdfUrl);
-      loadingTask.promise.then(pdfDocument => {
-
-        // request the first page only
-        return pdfDocument.getPage(1).then(pdfPage => {
-
-          // Display page on the existing canvas with 100% scale.
-          var viewport = pdfPage.getViewport(1.0);
-          var canvas = document.getElementById('the-canvas');
-
-          canvas.width = viewport.width;
-          canvas.height = viewport.height;
-          // canvas.width = 640;
-          // canvas.height = 480;
-
-          var ctx = canvas.getContext('2d');
-          var renderTask = pdfPage.render({
-            canvasContext: ctx,
-            viewport: viewport
-          });
-          return renderTask.promise;
-        });
-      }).catch(function (reason) {
-        console.error('Error: ' + reason);
-      });
-    }
   }
 
   handleAuth = (ev) => {
